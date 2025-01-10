8d14f87d4680b096fe88a08fd386763b9f1a9caa
Sjur Millidahl <sjur.millidahl@statnett.no>
2025-01-10 10:15:07 +0100

    refactor: introduce resty in cloudian sdk

--- internal/sdk/cloudian/sdk.go
+++ internal/sdk/cloudian/sdk.go
@@ -1,21 +1,19 @@
 package cloudian
 
 import (
-	"bytes"
 	"context"
 	"crypto/tls"
-	"encoding/json"
 	"errors"
 	"fmt"
-	"io"
-	"net/http"
 	"strconv"
+
+	"github.com/go-resty/resty/v2"
 )
 
 type Client struct {
-	baseURL    string
-	httpClient *http.Client
-	authHeader string
+	baseURL     string
+	restyClient *resty.Client
+	authHeader  string
 }
 
 type Group struct {
@@ -119,17 +117,16 @@ var ErrNotFound = errors.New("not found")
 // WithInsecureTLSVerify skips the TLS validation of the server certificate when `insecure` is true.
 func WithInsecureTLSVerify(insecure bool) func(*Client) {
 	return func(c *Client) {
-		c.httpClient = &http.Client{Transport: &http.Transport{
-			TLSClientConfig: &tls.Config{InsecureSkipVerify: insecure}, // nolint:gosec
-		}}
+		c.restyClient = resty.New().
+			SetTLSClientConfig(&tls.Config{InsecureSkipVerify: insecure}) //nolint:gosec
 	}
 }
 
 func NewClient(baseURL string, authHeader string, opts ...func(*Client)) *Client {
 	c := &Client{
-		baseURL:    baseURL,
-		httpClient: http.DefaultClient,
-		authHeader: authHeader,
+		baseURL:     baseURL,
+		restyClient: resty.New(),
+		authHeader:  authHeader,
 	}
 	for _, opt := range opts {
 		opt(c)
@@ -139,7 +136,7 @@ func NewClient(baseURL string, authHeader string, opts ...func(*Client)) *Client
 
 // List all users of a group.
 func (client Client) ListUsers(ctx context.Context, groupId string, offsetUserId *string) ([]User, error) {
-	var retVal []User
+	var retVal, users []User
 
 	limit := 100
 	params := map[string]string{
@@ -152,21 +149,14 @@ func (client Client) ListUsers(ctx context.Context, groupId string, offsetUserId
 		params["offset"] = *offsetUserId
 	}
 
-	resp, err := client.doRequest(ctx, http.MethodGet, "/user/list", params, nil)
+	_, err := client.restyClient.R().
+		SetHeader("Authorization", client.authHeader).
+		SetQueryParams(params).
+		SetResult(&users).
+		ForceContentType("application/json"). // TODO: why is this needed?
+		Get(client.baseURL + "/user/list")
 	if err != nil {
-		return nil, err
-	}
-
-	defer resp.Body.Close() // nolint:errcheck
-
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return nil, fmt.Errorf("GET reading list users response body failed: %w", err)
-	}
-
-	var users []User
-	if err := json.Unmarshal(body, &users); err != nil {
-		return nil, fmt.Errorf("GET unmarshal users response body failed: %w", err)
+		return nil, fmt.Errorf("GET list users failed: %w", err)
 	}
 
 	retVal = append(retVal, users...)
@@ -191,93 +181,69 @@ func (client Client) ListUsers(ctx context.Context, groupId string, offsetUserId
 
 // Delete a single user. Errors if the user does not exist.
 func (client Client) DeleteUser(ctx context.Context, user User) error {
-	resp, err := client.doRequest(ctx, http.MethodDelete, "/user",
-		map[string]string{"groupId": user.GroupID, "userId": user.UserID}, nil)
+	_, err := client.restyClient.R().
+		SetHeader("Authorization", client.authHeader).
+		SetQueryParams(map[string]string{
+			"groupId": user.GroupID,
+			"userId":  user.UserID,
+		}).
+		SetError(&ErrNotFound). // TODO test this behaviour
+		Delete(client.baseURL + "/user")
 	if err != nil {
 		return err
 	}
-	defer resp.Body.Close() // nolint:errcheck
 
-	switch resp.StatusCode {
-	case 200:
-		return nil
-	default:
-		return fmt.Errorf("DELETE unexpected status. Failure: %d", resp.StatusCode)
-	}
+	return nil
 
 }
 
 // Create a single user of type `User` into a groupId
 func (client Client) CreateUser(ctx context.Context, user User) error {
-	jsonData, err := json.Marshal(toInternalUser(user))
-	if err != nil {
-		return fmt.Errorf("error marshaling JSON: %w", err)
-	}
+	_, err := client.restyClient.R().
+		SetHeader("Authorization", client.authHeader).
+		SetBody(toInternalUser(user)).
+		Put(client.baseURL + "/user")
 
-	resp, err := client.doRequest(ctx, http.MethodPut, "/user", nil, jsonData)
-	if err != nil {
-		return err
-	}
-
-	return resp.Body.Close()
+	return err
 }
 
 // CreateUserCredentials creates a new set of credentials for a user.
 func (client Client) CreateUserCredentials(ctx context.Context, user User) (*SecurityInfo, error) {
-	resp, err := client.doRequest(ctx, http.MethodPut, "/user/credentials",
-		map[string]string{"groupId": user.GroupID, "userId": user.UserID}, nil)
+	var securityInfo SecurityInfo
+	_, err := client.restyClient.R().
+		SetHeader("Authorization", client.authHeader).
+		ForceContentType("application/json"). // TODO: why is this needed?
+		SetResult(&securityInfo).
+		SetBody(map[string]string{"groupId": user.GroupID, "userId": user.UserID}).
+		Put(client.baseURL + "/user/credentials")
 	if err != nil {
 		return nil, err
 	}
 
-	defer resp.Body.Close() // nolint:errcheck
-
-	switch resp.StatusCode {
-	case 200:
-		body, err := io.ReadAll(resp.Body)
-		if err != nil {
-			return nil, fmt.Errorf("error reading create credentials response: %w", err)
-		}
-
-		var securityInfo SecurityInfo
-		if err := json.Unmarshal(body, &securityInfo); err != nil {
-			return nil, fmt.Errorf("error parsing create credentials response: %w", err)
-		}
-
-		return &securityInfo, nil
-	default:
-		return nil, fmt.Errorf("error: create credentials unexpected status code: %d", resp.StatusCode)
-	}
+	return &securityInfo, nil
 }
 
 // GetUserCredentials fetches all the credentials of a user.
 func (client Client) GetUserCredentials(ctx context.Context, user User) ([]SecurityInfo, error) {
-	resp, err := client.doRequest(ctx, http.MethodGet, "/user/credentials/list",
-		map[string]string{"groupId": user.GroupID, "userId": user.UserID}, nil)
+	var securityInfos []SecurityInfo
+
+	resp, err := client.restyClient.R().
+		SetHeader("Authorization", client.authHeader).
+		SetQueryParams(map[string]string{"groupId": user.GroupID, "userId": user.UserID}).
+		ForceContentType("application/json"). // TODO: why is this needed?
+		SetResult(&securityInfos).
+		Get(client.baseURL + "/user/credentials/list")
 	if err != nil {
 		return nil, err
 	}
-
-	defer resp.Body.Close() // nolint:errcheck
-
-	switch resp.StatusCode {
+	switch resp.StatusCode() {
 	case 200:
-		body, err := io.ReadAll(resp.Body)
-		if err != nil {
-			return nil, fmt.Errorf("error reading credentials response: %w", err)
-		}
-
-		var securityInfo []SecurityInfo
-		if err := json.Unmarshal(body, &securityInfo); err != nil {
-			return nil, fmt.Errorf("error parsing credentials response: %w", err)
-		}
-
-		return securityInfo, nil
+		return securityInfos, nil
 	case 204:
 		// Cloudian-API returns 204 if no security credentials found
 		return nil, ErrNotFound
 	default:
-		return nil, fmt.Errorf("error: list credentials unexpected status code: %d", resp.StatusCode)
+		return nil, fmt.Errorf("error: list credentials errored %w with unexpected status code (%d)", err, resp.StatusCode())
 	}
 }
 
@@ -299,69 +265,50 @@ func (client Client) DeleteGroupRecursive(ctx context.Context, groupId string) e
 
 // Deletes a group if it is without members.
 func (client Client) DeleteGroup(ctx context.Context, groupId string) error {
-	resp, err := client.doRequest(ctx, http.MethodDelete, "/group",
-		map[string]string{"groupId": groupId}, nil)
-	if err != nil {
-		return err
-	}
+	_, err := client.restyClient.R().
+		SetHeader("Authorization", client.authHeader).
+		SetQueryParams(map[string]string{"groupId": groupId}).
+		Delete(client.baseURL + "/group")
 
-	return resp.Body.Close()
+	return err
 }
 
 // Creates a group.
 func (client Client) CreateGroup(ctx context.Context, group Group) error {
-	jsonData, err := json.Marshal(toInternal(group))
-	if err != nil {
-		return fmt.Errorf("error marshaling JSON: %w", err)
-	}
+	_, err := client.restyClient.R().
+		SetHeader("Authorization", client.authHeader).
+		SetBody(toInternal(group)).
+		Put(client.baseURL + "/group")
 
-	resp, err := client.doRequest(ctx, http.MethodPut, "/group", nil, jsonData)
-	if err != nil {
-		return err
-	}
-
-	return resp.Body.Close()
+	return err
 }
 
 // Updates a group if it does not exists.
 func (client Client) UpdateGroup(ctx context.Context, group Group) error {
-	jsonData, err := json.Marshal(toInternal(group))
-	if err != nil {
-		return fmt.Errorf("error marshaling JSON: %w", err)
-	}
+	_, err := client.restyClient.R().
+		SetHeader("Authorization", client.authHeader).
+		SetBody(toInternal(group)).
+		Post(client.baseURL + "/group")
 
-	// Create a context with a timeout
-	resp, err := client.doRequest(ctx, http.MethodPost, "/group", nil, jsonData)
-	if err != nil {
-		return err
-	}
-
-	return resp.Body.Close()
+	return err
 }
 
 // Get a group. Returns an error even in the case of a group not found.
 // This error can then be checked against ErrNotFound: errors.Is(err, ErrNotFound)
 func (client Client) GetGroup(ctx context.Context, groupId string) (*Group, error) {
-	resp, err := client.doRequest(ctx, http.MethodGet, "/group",
-		map[string]string{"groupId": groupId}, nil)
+	var group groupInternal
+	resp, err := client.restyClient.R().
+		SetHeader("Authorization", client.authHeader).
+		SetQueryParams(map[string]string{"groupId": groupId}).
+		ForceContentType("application/json"). // TODO: why is this needed?
+		SetResult(&group).
+		Get(client.baseURL + "/group")
 	if err != nil {
 		return nil, err
 	}
 
-	defer resp.Body.Close() // nolint:errcheck
-
-	switch resp.StatusCode {
+	switch resp.StatusCode() {
 	case 200:
-		body, err := io.ReadAll(resp.Body)
-		if err != nil {
-			return nil, fmt.Errorf("GET reading response body failed: %w", err)
-		}
-
-		var group groupInternal
-		if err := json.Unmarshal(body, &group); err != nil {
-			return nil, fmt.Errorf("GET unmarshal response body failed: %w", err)
-		}
-
 		retVal := fromInternal(group)
 		return &retVal, nil
 	case 204:
@@ -370,27 +317,4 @@ func (client Client) GetGroup(ctx context.Context, groupId string) (*Group, erro
 	default:
 		return nil, fmt.Errorf("GET unexpected status. Failure: %w", err)
 	}
-}
-
-func (client Client) doRequest(ctx context.Context, method string, url string, query map[string]string, body []byte) (*http.Response, error) {
-	var buffer io.Reader = nil
-	if body != nil {
-		buffer = bytes.NewBuffer(body)
-	}
-	req, err := http.NewRequestWithContext(ctx, method, client.baseURL+url, buffer)
-	if err != nil {
-		return nil, fmt.Errorf("error creating request: %w", err)
-	}
-
-	req.Header.Set("Accept", "application/json")
-	req.Header.Set("Content-Type", "application/json")
-	req.Header.Set("Authorization", client.authHeader)
-
-	q := req.URL.Query()
-	for k, v := range query {
-		q.Set(k, v)
-	}
-	req.URL.RawQuery = q.Encode()
-
-	return client.httpClient.Do(req)
 }
